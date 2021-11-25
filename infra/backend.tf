a2eff178c81b5a43fe202019ecc12e6e7b0c1c9b
Coin Computer <nospaceleftondevice@gmail.com>
2021-11-25 04:36:33 -0500

    remove duplicate infra directory

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "liberty-state-bucket"
-    key    = "liberty-infra/terraform.tfstate"
-    region = "us-east-2"
-    dynamodb_table = "terraform-state-lock-dynamo"
-  }
-}
